9d68eb2d522c6b2bae7f2461a29e11fb2dc56eca
Peng_ziyu <t109590042@ntut.org.tw>
2022-04-03 04:37:19 +0800

    4/3

---
+++ Jump King/Source/GameAudio.h
@@ -0,0 +1,10 @@
+namespace game_framework {
+
+	enum AUDIO_ID {				// 定義各種音效的編號
+		AUDIO_Menu_Intro,		// 0 進logo音樂
+		AUDIO_Opening_Theme,	// 1 按Enter後音樂
+		AUDIO_Fall,
+		AUDIO_Strong_Fall,
+		AUDIO_Bump
+	};
+}
